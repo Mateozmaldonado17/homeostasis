e4a1bb849c7bf4d5021b3d52b1a5c37e274af0dd
Mateo Zapata <mszado15@gmail.com>
2024-12-09 07:13:21 -0500

    removed unnecesary custom validation

--- src/utils/string/validate-and-suggest-naming-convention.ts
+++ src/utils/string/validate-and-suggest-naming-convention.ts
@@ -1,6 +1,5 @@
 import { ConventionList } from '../../models/IDescriptor';
 import { isCamelCase, toCamelCase } from "./camel-case";
-import { isValidRegex, validateWithRegex } from "./regex-functions";
 import { isKebabCase, toKebabCase } from "./kebab-case";
 import { isPascalCase, toPascalCase } from "./pascal-case";
 import { isSnakeCase, toSnakeCase } from "./snake-case";
@@ -25,13 +24,6 @@ export function validateAndSuggestNamingConvention(
   if (conventionFormat in standardFormats) {
     const { isValid, toFormat } = standardFormats[conventionFormat];
     return { isValid: isValid(name), suggestedName: toFormat(name) };
-  }
-  
-  if (isValidRegex(conventionFormat)) {
-    return {
-      isValid: validateWithRegex(conventionFormat, name),
-      suggestedName: name.replace(new RegExp(conventionFormat, "g"), ""),
-    };
   }
 
   throw new Error(`Unsupported convention: ${conventionFormat}`);
